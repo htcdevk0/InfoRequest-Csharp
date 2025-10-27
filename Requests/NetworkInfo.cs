a47edf0dddada7d014dc11cbaa97a66ac3fb93c4
htcdevk0 <htcdevk0@outlook.com>
2025-10-26 23:55:07 -0300

    Update NetworkInfo.cs

--- Requests/NetworkInfo.cs
+++ Requests/NetworkInfo.cs
@@ -51,3 +51,52 @@ namespace InfoRequest.Requests
         public static string RequestHostName = Dns.GetHostName();
     }
 }
+namespace InfoRequest.Requests
+{
+    public class NetworkInfo
+    {
+        public static string RequestLocalIP
+        {
+            get
+            {
+                try
+                {
+                    var host = Dns.GetHostEntry(Dns.GetHostName());
+                    foreach (var ip in host.AddressList)
+                    {
+                        if (ip.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            return ip.ToString();
+                        }
+                    }
+                    return "No IPv4 address found";
+                }
+                catch
+                {
+                    return "Error retrieving local IP";
+                }
+            }
+        }
+        
+        public static string RequestPublicIP
+        {
+            get
+            {
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        return client.DownloadString("https://api.ipify.org");
+                    }
+                }
+                catch
+                {
+                    return "Error retrieving public IP";
+                }
+            }
+        }
+
+        // Nome do host
+        public static string RequestHostName = Dns.GetHostName();
+    }
+}

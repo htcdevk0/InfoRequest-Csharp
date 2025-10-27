4d773a4239595ac3e8132f8b31e73e9ab4e624eb
htcdevk0 <htcdevk0@outlook.com>
2025-10-26 23:54:20 -0300

    Update RequestConsoleColor.cs

--- RequestMisc/RequestConsoleColor.cs
+++ RequestMisc/RequestConsoleColor.cs
@@ -1,4 +1,12 @@
 ﻿namespace InfoRequest.RequestMisc;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Security.Principal;
+using System.Management;
+using System.Diagnostics;
+using System.Reflection;
 
 public class RequestConsoleColor
 {
